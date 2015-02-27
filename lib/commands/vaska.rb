97b0a5abbf5bbffb886b8be24e43d59a5859f80e
Jonas Brusman <himynameisjonas@gmail.com>
2015-02-27 11:56:47 +0100

    Jag vaskar mitt liv

---
+++ lib/commands/vaska.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+require_relative 'sound_command'
+class Vaska < SoundCommand
+  respond_to "vaska", "liv"
+  listen_to /vaska/i
+
+  FULL = "vaska/vaska.mp3"
+  FILES = Dir.glob("extras/vaska/vaska_short*.mp3").map{|f| f.gsub("extras/","")}
+
+  def self.description
+    "Jag vaskar mitt liv!"
+  end
+
+  def self.respond(message)
+    play_file audio_file
+  end
+
+  def self.listen(message)
+    play_file audio_file
+  end
+
+  def self.audio_file
+    rand(100) > 80 ? FULL : FILES.sample
+  end
+end
+
